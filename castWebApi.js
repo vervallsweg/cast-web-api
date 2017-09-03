e6c09a9255b38bc257eb0ed75ec19dba7dfa955e
Tobias Haerke <tosty@outlook.com>
2017-09-03 16:23:03 +0200

    Fixed package.json, version bump

--- castWebApi.js
+++ castWebApi.js
@@ -11,7 +11,7 @@ var currentRequestId = 1;
 var networkTimeout = 2000;
 var discoveryTimeout = 4000;
 var appLoadTimeout = 6000;
-var thisVersion = 0.2;
+var thisVersion = '0.2.1';
 
 interpretArguments();
 createWebServer();
